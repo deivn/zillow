9f5e6ee7994dc39802145a71e13203895099d6c2
taoyi <wh429004@126.com>
2019-07-01 11:40:34 +0800

    commit

--- redis2mysql.py
+++ redis2mysql.py
@@ -74,42 +74,25 @@ class Redis2Mysql(object):
 
     """"
     功能： 获取图片，如果不包含谷歌地图图片，则判断，如果图片只有一张，主图辅图共用同一张。如果多张，则取第一张为主图，剩余为辅图
-    如果包含谷歌图片，则将包含的谷歌图片删除，取剩余，逻辑同上
+    如果包含谷歌图片，则去除
     """
     def get_img(self, imges, rex):
         if imges:
             if rex in imges:
-                _imgs = imges.split(",")
-                for img in _imgs:
-                    if rex in img:
-                        _imgs.remove(img)
-                return self.img_deal(_imgs)
+                return ()
             else:
                 return self.img_deal(imges)
         return ()
 
     """"功能：如果图片只有一张，则主图和辅图同为一张，如果为多张，则取第一张为主图，剩余为辅图"""
     def img_deal(self, imgs):
-        # 列表
-        if not isinstance(imgs, str):
-            img_len = len(imgs)
-            if img_len:
-                if img_len == 1:
-                    return (imgs[0], imgs[0])
-                else:
-                    imges = ",".join(imgs)
-                    first = imges[:imges.find(',')]
-                    less = imges[imges.find(',') + 1:]
-                    return (first, less)
-        else:
-            # 字符串不包含谷歌图片并且长度大小为1和大余1的情况
-            _imgs = imgs.split(",")
-            if len(_imgs) == 1:
-                return (_imgs[0], _imgs[0])
-            first = imgs[:imgs.find(',')]
-            less = imgs[imgs.find(',') + 1:]
-            return (first, less)
-        return ()
+        # 字符串不包含谷歌图片并且长度大小为1和大余1的情况
+        _imgs = imgs.split(",")
+        if len(_imgs) == 1:
+            return (_imgs[0], _imgs[0])
+        first = imgs[:imgs.find(',')]
+        less = imgs[imgs.find(',') + 1:]
+        return (first, less)
 
     def get_dealtype(self, dealtype):
         _dealtype = dealtype.lower()
@@ -137,14 +120,16 @@ def main():
         item = {}
         imgs = redis2mysql.get_img(_item['img_url'], 'maps.googleapis.com')
         contact_phone = _item['contact_phone'] if _item['contact_phone'] else ""
-        # 不含图片，不含手机号则不入库
-        if imgs and contact_phone:
+        house_type_id = redis2mysql.get_housetypeid(_item['house_type'])
+        lot_sqft = _item['lot_sqft'] if _item['lot_sqft'] and _item['lot_sqft'] != -1.0 else 0
+        # 按图片，手机号,房源类型,占地面积过滤
+        if imgs and contact_phone and house_type_id and lot_sqft:
             # 使用execute方法执行SQL INSERT语
             item['house_id'] = redis2mysql.get_houseid(_item['street'])
-            item['user_id'] = '20190629'
+            item['user_id'] = '84737239254302720'
             item['state_id'] = _item['state']
             item['city_id'] = redis2mysql.get_cid(_item['state'], _item['city'])
-            item['house_type_id'] = redis2mysql.get_housetypeid(_item['house_type'])
+            item['house_type_id'] = house_type_id
             item['price'] = _item['price']
             item['hoa_fee'] = _item['hoa_fee'] if _item['hoa_fee'] and _item['hoa_fee'] != 'No' else "0"
             item['mls'] = _item['mls'] if _item['mls'] and _item['mls'] != 'No' else ""
@@ -159,15 +144,16 @@ def main():
             item['living_sqft'] = _item['living_sqft'] if _item['living_sqft'] and _item['living_sqft'] != -1.0 else 0
             item['latitude'] = _item['latitude']
             item['longitude'] = _item['longitude']
-            item['year_build'] = _item['year_build'] if _item['year_build'] else ""
+            item['year_build'] = _item['year_build'] if _item['year_build'] else "2015"
             first, less = imgs
             item['img_url'] = first
             item['deal_type'] = redis2mysql.get_dealtype(_item['deal_type'])
             item['deposit'] = redis2mysql.get_deposit(_item['deposit'])
             item['contact_name'] = _item['contact_name'] if _item['contact_name'] else ""
             item['contact_phone'] = contact_phone
-            item['contact_email'] = '20190629@gmail.com'
-            item['create_time'] = random.randint(1560480030, 1561891667)
+            item['contact_email'] = '2991142350@qq.com'
+            # 2014-01-01 01:03:05 ----->  2019-07-01 01:03:05
+            item['create_time'] = random.randint(1388509385, 1561914185)
             item['url'] = _item['url']
             # -------------------------------house_detail----------------
             item['house_img'] = less
@@ -184,7 +170,7 @@ def main():
                             %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
                             [item['house_id'], item['user_id'], item['state_id'], item['city_id'], item['house_type_id'], item['price'], item['hoa_fee'], item['mls'], item['apn'], item['street'],
                              item['zip'], item['bedroom'], item['bathroom'], item['garage'], item['lot_sqft'], item['user_input_unit'], item['living_sqft'], item['latitude'], item['longitude'],
-                            item['year_build'], item['img_url'], 1, 0, item['deal_type'], 1, 1, 1, 0, 1,
+                            item['year_build'], item['img_url'], 20190701, 0, item['deal_type'], 1, 1, 1, 0, 1,
                             item['deposit'], item['contact_name'], item['contact_phone'], item['contact_email'], 2, item['create_time'], "", 0, 1, "", 0, item['url']])
                 if count == 1:
                     cur.execute("INSERT ignore t_house_detail_new (house_id, house_img, house_video, house_desc, house_deed, house_amenities, deny_reason, view_count, house_amenities_value) VALUES (\
